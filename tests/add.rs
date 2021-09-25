4c0dc0d581aec9ca07c8b50e53debd4761918533
zkud <kudirko.anton@gmail.com>
2021-09-25 15:22:48 +0300

    Fixed lint issues

--- tests/add.rs
+++ tests/add.rs
@@ -2,12 +2,12 @@ use rfraction::Fraction;
 
 #[test]
 fn with_usual_nums_add_works() {
-  let first = Fraction::new(10, 140, false);
-  let second = Fraction::new(15, 280, false);
+    let first = Fraction::new(10, 140, false);
+    let second = Fraction::new(15, 280, false);
 
-  let result = first.add(&second);
+    let result = first.add(&second);
 
-  assert!(result.is_positive());
-  assert_eq!(result.numerator(), 1);
-  assert_eq!(result.denominator(), 8);
+    assert!(result.is_positive());
+    assert_eq!(result.numerator(), 1);
+    assert_eq!(result.denominator(), 8);
 }
