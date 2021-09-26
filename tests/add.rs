e76acf13b718c9b9cc94ffebac5472ed9fa09deb
zkud <kudirko.anton@gmail.com>
2021-09-26 13:20:10 +0300

    Provided some tests for the add operation

--- tests/add.rs
+++ tests/add.rs
@@ -11,3 +11,66 @@ fn with_usual_nums_add_works() {
   assert_eq!(result.numerator(), 1);
   assert_eq!(result.denominator(), 8);
 }
+
+#[test]
+fn with_nans_nums_add_works() {
+  let nan_number = Fraction::<u128>::new_nan();
+  let default_number = Fraction::<u128>::new_natural(123);
+  let inf = Fraction::<u128>::new_positive_infinity();
+
+  let result = nan_number.add(&nan_number);
+  assert!(result.is_nan());
+
+  let result = nan_number.add(&default_number);
+  assert!(result.is_nan());
+
+  let result = default_number.add(&nan_number);
+  assert!(result.is_nan());
+
+  let result = nan_number.add(&nan_number);
+  assert!(result.is_nan());
+
+  let result = inf.add(&nan_number);
+  assert!(result.is_nan());
+}
+
+#[test]
+fn with_infinities_nums_add_works() {
+  let default_number = Fraction::<u128>::new_natural(123);
+  let positive_inf = Fraction::<u128>::new_positive_infinity();
+  let negative_inf = Fraction::<u128>::new_negative_infinity();
+
+  let result = default_number.add(&positive_inf);
+  assert!(result.is_positive_infinity());
+
+  let result = default_number.add(&negative_inf);
+  assert!(result.is_negative_infinity());
+
+  let result = positive_inf.add(&negative_inf);
+  assert!(result.is_nan());
+
+  let result = positive_inf.add(&positive_inf);
+  assert!(result.is_positive_infinity());
+
+  let result = negative_inf.add(&negative_inf);
+  assert!(result.is_negative_infinity());
+}
+
+#[test]
+fn with_zeros_nums_add_works() {
+  let default_number = Fraction::<u128>::new_natural(123);
+  let zero = Fraction::<u128>::new_zero();
+
+  let result = default_number.add(&zero);
+  assert_eq!(result.numerator(), 123);
+  assert_eq!(result.denominator(), 1);
+  assert!(result.is_positive());
+
+  let result = zero.add(&default_number);
+  assert_eq!(result.numerator(), 123);
+  assert_eq!(result.denominator(), 1);
+  assert!(result.is_positive());
+
+  let result = zero.add(&zero);
+  assert!(result.is_zero());
+}
