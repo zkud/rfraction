d3a53e7b0ad899a27181746b46c8502cae380d8a
zkud <kudirko.anton@gmail.com>
2021-09-26 13:28:31 +0300

    Splitted functionality into modules

---
+++ src/unsigned_number.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+use std::ops;
+
+pub trait UnsignedNumber:
+  fmt::Display
+  + Copy
+  + Clone
+  + PartialEq
+  + From<u8>
+  + PartialOrd
+  + ops::Add<Output = Self>
+  + ops::Sub<Output = Self>
+  + ops::Mul<Output = Self>
+  + ops::Div<Output = Self>
+  + ops::Rem<Output = Self>
+{
+  fn checked_add(self, other: Self) -> Option<Self>;
+  fn checked_sub(self, other: Self) -> Option<Self>;
+  fn checked_mul(self, other: Self) -> Option<Self>;
+  fn checked_div(self, other: Self) -> Option<Self>;
+  fn checked_rem(self, other: Self) -> Option<Self>;
+}
+
+impl UnsignedNumber for u128 {
+  fn checked_add(self, other: Self) -> Option<Self> {
+    self.checked_add(other)
+  }
+
+  fn checked_mul(self, other: Self) -> Option<Self> {
+    self.checked_mul(other)
+  }
+
+  fn checked_div(self, other: Self) -> Option<Self> {
+    self.checked_div(other)
+  }
+
+  fn checked_sub(self, other: Self) -> Option<Self> {
+    self.checked_sub(other)
+  }
+
+  fn checked_rem(self, other: Self) -> Option<Self> {
+    self.checked_rem(other)
+  }
+}
