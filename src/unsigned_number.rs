fbba1a1ea1a5d836912bd280d22ff2e9dcdbb65b
zkud <kudirko.anton@gmail.com>
2021-09-26 13:29:58 +0300

    Added support for various unsigned types

--- src/unsigned_number.rs
+++ src/unsigned_number.rs
@@ -42,3 +42,91 @@ impl UnsignedNumber for u128 {
     self.checked_rem(other)
   }
 }
+
+impl UnsignedNumber for u64 {
+  fn checked_add(self, other: Self) -> Option<Self> {
+    self.checked_add(other)
+  }
+
+  fn checked_mul(self, other: Self) -> Option<Self> {
+    self.checked_mul(other)
+  }
+
+  fn checked_div(self, other: Self) -> Option<Self> {
+    self.checked_div(other)
+  }
+
+  fn checked_sub(self, other: Self) -> Option<Self> {
+    self.checked_sub(other)
+  }
+
+  fn checked_rem(self, other: Self) -> Option<Self> {
+    self.checked_rem(other)
+  }
+}
+
+impl UnsignedNumber for u32 {
+  fn checked_add(self, other: Self) -> Option<Self> {
+    self.checked_add(other)
+  }
+
+  fn checked_mul(self, other: Self) -> Option<Self> {
+    self.checked_mul(other)
+  }
+
+  fn checked_div(self, other: Self) -> Option<Self> {
+    self.checked_div(other)
+  }
+
+  fn checked_sub(self, other: Self) -> Option<Self> {
+    self.checked_sub(other)
+  }
+
+  fn checked_rem(self, other: Self) -> Option<Self> {
+    self.checked_rem(other)
+  }
+}
+
+impl UnsignedNumber for u16 {
+  fn checked_add(self, other: Self) -> Option<Self> {
+    self.checked_add(other)
+  }
+
+  fn checked_mul(self, other: Self) -> Option<Self> {
+    self.checked_mul(other)
+  }
+
+  fn checked_div(self, other: Self) -> Option<Self> {
+    self.checked_div(other)
+  }
+
+  fn checked_sub(self, other: Self) -> Option<Self> {
+    self.checked_sub(other)
+  }
+
+  fn checked_rem(self, other: Self) -> Option<Self> {
+    self.checked_rem(other)
+  }
+}
+
+impl UnsignedNumber for u8 {
+  fn checked_add(self, other: Self) -> Option<Self> {
+    self.checked_add(other)
+  }
+
+  fn checked_mul(self, other: Self) -> Option<Self> {
+    self.checked_mul(other)
+  }
+
+  fn checked_div(self, other: Self) -> Option<Self> {
+    self.checked_div(other)
+  }
+
+  fn checked_sub(self, other: Self) -> Option<Self> {
+    self.checked_sub(other)
+  }
+
+  fn checked_rem(self, other: Self) -> Option<Self> {
+    self.checked_rem(other)
+  }
+}
