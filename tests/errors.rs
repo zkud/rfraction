6d2968d08eb336d80b2a500185ad67fc5723b406
zkud <kudirko.anton@gmail.com>
2021-09-30 20:29:49 +0300

    Provided additional tests for errors

--- tests/errors.rs
+++ tests/errors.rs
@@ -16,3 +16,18 @@ fn it_inits() {
   assert_eq!(overflow_error.message(), "operation error");
   assert_eq!(overflow_error.error_type(), OperationErrorType::Overflow);
 }
+
+#[test]
+fn it_displayble() {
+  let division_error = OperationError::new("division error", OperationErrorType::DivisionByZero);
+  assert_eq!(
+    format!("{}", division_error),
+    String::from("error caused by division by zero, reason: division error")
+  );
+
+  let overflow_error = OperationError::new("operation error", OperationErrorType::Overflow);
+  assert_eq!(
+    format!("{}", overflow_error),
+    String::from("error caused by overflow error, reason: operation error")
+  );
+}
