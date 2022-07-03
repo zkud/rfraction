b5d8c9264ac9a44cba36baba018f8113ed90305a
zkud <anton.kudyrka@gmail.com>
2022-07-03 15:47:11 +0300

    Fixed linting

--- src/fraction/abs.rs
+++ src/fraction/abs.rs
@@ -5,9 +5,9 @@ use super::Fraction;
 
 impl<N: UnsignedNumber> Fraction<N> {
   /// Returns an absolute value of the given number
-  /// 
+  ///
   /// ### Examples
-  /// 
+  ///
   /// ```rust
   /// use rfraction::{Fraction, Sign};
   /// let sign = Sign::Negative;
@@ -16,15 +16,15 @@ impl<N: UnsignedNumber> Fraction<N> {
   /// let number = Fraction::<u128>::new(sign, numerator, denominator);
   /// let another_number = Fraction::<u128>::new(sign.inverse(), numerator, denominator);
   /// assert_eq!(number.abs(), another_number.abs());
-  /// ``` 
+  /// ```
   pub fn abs(&self) -> Fraction<N> {
     self.try_abs().unwrap()
   }
 
   /// Returns an absolute value of the given number or could fail with an [`OperationError`]
-  /// 
+  ///
   /// ### Examples
-  /// 
+  ///
   /// ```rust
   /// use rfraction::{Fraction, Sign};
   /// let sign = Sign::Negative;
@@ -33,11 +33,11 @@ impl<N: UnsignedNumber> Fraction<N> {
   /// let number = Fraction::<u128>::new(sign, numerator, denominator);
   /// let another_number = Fraction::<u128>::new(sign.inverse(), numerator, denominator);
   /// assert_eq!(number.abs(), another_number.abs());
-  /// ``` 
-  /// 
+  /// ```
+  ///
   /// ### Errors
   ///
-  /// If there are some issues with the number, 
+  /// If there are some issues with the number,
   /// it will return a [`OperationError`]
   pub fn try_abs(&self) -> Result<Fraction<N>, OperationError> {
     Fraction::try_new(Sign::Positive, self.numerator(), self.denominator())
