3c4e90053903c19b7b83b9c82fe6589d987cbe03
zkud <anton.kudyrka@gmail.com>
2022-07-03 17:06:24 +0300

    Added docstrings for addition

--- src/fraction/add.rs
+++ src/fraction/add.rs
@@ -5,10 +5,58 @@ use super::Fraction;
 use std::ops;
 
 impl<N: UnsignedNumber> Fraction<N> {
+  /// Adds two numbers and returns a sum
+  ///
+  /// ### Examples
+  /// ```rust
+  /// use rfraction::{Fraction, Sign};
+  ///
+  /// let sign = Sign::Positive;
+  /// let numerator = 10;
+  /// let denominator = 140;
+  /// let a = Fraction::<u128>::new(sign, numerator, denominator);
+  /// let numerator = 15;
+  /// let denominator = 280;
+  /// let b = Fraction::<u128>::new(sign, numerator, denominator);
+  ///
+  /// let result = a.add(&b);
+  ///
+  /// assert!(result.is_positive());
+  /// assert_eq!(result.numerator(), 1);
+  /// assert_eq!(result.denominator(), 8);
+  /// ```
+  ///
+  /// ### Panics
+  ///
+  /// If some errors occur during addition, for example overflow is encountered,
+  /// it will panic
   pub fn add(&self, other: &Self) -> Self {
     self.try_add(other).unwrap()
   }
 
+  /// Adds two numbers and returns a sum or could fail with an [`OperationError`]
+  ///
+  /// ### Examples
+  /// ```rust
+  /// use rfraction::{Fraction, Sign, OperationError};
+  ///
+  /// let sign = Sign::Positive;
+  /// let numerator = 10;
+  /// let denominator = 140;
+  /// let a = Fraction::<u8>::new(sign, numerator, denominator);
+  /// let numerator = 3;
+  /// let denominator = 56;
+  /// let b = Fraction::<u8>::new(sign, numerator, denominator);
+  ///
+  /// let result = a.add(&b);
+  ///
+  /// assert!(matches!(result.is_positive(), OperationError));
+  /// ```
+  ///
+  /// ### Errors
+  ///
+  /// If some errors occur during addition, for example overflow is encountered,
+  /// it will return a [`OperationError`]
   pub fn try_add(&self, other: &Self) -> Result<Self, OperationError> {
     let (this, other) = self.unify(other)?;
     let sign = this.get_result_sign(&other);
